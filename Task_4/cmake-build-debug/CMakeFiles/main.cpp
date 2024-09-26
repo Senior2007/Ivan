1b30b8154e6ed39eec4e5ab3454980e6acd9ce7a
Senior2007 <154319944+Senior2007@users.noreply.github.com>
2024-09-26 15:32:12 +0300

    Add files via upload

---
+++ Task_4/cmake-build-debug/CMakeFiles/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    int x, y;
+    double a, b, c, k;
+    std::cin >> x >> y >> a >> b >> c >> k;
+
+    if (x == y) {
+        x = 0;
+        y = 0;
+    } else {
+        if (x > y) y = 0; else x = 0;
+    }
+
+    if (a > b && a > c) {
+        a -= k;
+    } else {
+        if (b > a && b > c) {
+            b -= k;
+        } else {
+            c -= k;
+        }
+    }
+
+    std::cout << x << ' ' << y << ' ' <<a << ' ' << b << ' ' << c;
+
+    return 0;
+}
